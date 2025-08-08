5870141082db4922ceaa1a0ad3f048e5d0840b22
MONISH. P <158838702+MONISH2502@users.noreply.github.com>
2025-08-08 19:34:09 +0530

    Update 12-decimalPart.py
    Modified the code, so that the precise decimal part shall be printed

--- 01-BridgeCourse/PythonProgramming/12-decimalPart.py
+++ 01-BridgeCourse/PythonProgramming/12-decimalPart.py
@@ -1,10 +1,8 @@
 num = float(input("Enter a float number : "))
 
-real_num = float(int(num))
+st = str(num) 
 
-round(num,3)
-
-ans = round(num - real_num,3)
-print(ans)
+ans = st.split("." [-1]) 
+print(ans) 
 
 
